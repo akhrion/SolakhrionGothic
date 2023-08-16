c96e202280e5203da20573f7febd1943b1b3f8f5
Maxim <74652627+akhrion@users.noreply.github.com>
2023-08-16 15:45:57 +0300

    Правка: Поправлено появление надписи о невозможности торговли

--- PrjGothic/_misk_/g_cannottrade.d
+++ PrjGothic/_misk_/g_cannottrade.d
@@ -1,8 +1,10 @@
 
 func void g_cannottrade()
 {
+	Print("g_cannottrade");
+	
 	var C_Npc trader;
-	PrintScreen(_STR_MESSAGE_TRADE_FAILURE,-1,65,"font_old_10_white.tga",2);
+	PrintScreen(_STR_MESSAGE_TRADE_FAILURE,-1,60,"font_old_10_white.tga",2);
 	trader = Hlp_GetNpc(hero.aivar[AIV_LASTTARGET]);
 	B_Say(trader,hero,"$YOUWANNAFOOLME");
 };
