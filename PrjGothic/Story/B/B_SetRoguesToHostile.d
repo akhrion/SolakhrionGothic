3f94195db4c03454cf8ecad1cb15fab453bae7a5
Maxim <74652627+akhrion@users.noreply.github.com>
2023-08-30 13:42:47 +0300

    начата линейка бандитов

--- PrjGothic/Story/B/B_SetRoguesToHostile.d
+++ PrjGothic/Story/B/B_SetRoguesToHostile.d
@@ -8,17 +8,22 @@ func void B_SetRoguesToHostile()
 	var C_Npc weg1502;
 	var C_Npc weg1503;
 	var C_Npc weg1504;
-	weg1501 = Hlp_GetNpc(Non_1501_Wegelagerer);
-	Npc_SetTempAttitude(weg1501,ATT_HOSTILE);
-	Npc_SetAttitude(weg1501,ATT_HOSTILE);
-	weg1502 = Hlp_GetNpc(Non_1502_Wegelagerer);
-	Npc_SetTempAttitude(weg1502,ATT_HOSTILE);
-	Npc_SetAttitude(weg1502,ATT_HOSTILE);
-	weg1503 = Hlp_GetNpc(Non_1503_Wegelagerer);
-	Npc_SetTempAttitude(weg1503,ATT_HOSTILE);
-	Npc_SetAttitude(weg1503,ATT_HOSTILE);
-	weg1504 = Hlp_GetNpc(Non_1504_Wegelagerer);
-	Npc_SetTempAttitude(weg1504,ATT_HOSTILE);
-	Npc_SetAttitude(weg1504,ATT_HOSTILE);
+	if(Random_RogueIsAttack(PC_iRogueInvolvements))
+	{
+		weg1501 = Hlp_GetNpc(Non_1501_Wegelagerer);
+		Npc_SetTempAttitude(weg1501,ATT_HOSTILE);
+		Npc_SetAttitude(weg1501,ATT_HOSTILE);
+		weg1502 = Hlp_GetNpc(Non_1502_Wegelagerer);
+		Npc_SetTempAttitude(weg1502,ATT_HOSTILE);
+		Npc_SetAttitude(weg1502,ATT_HOSTILE);
+		weg1503 = Hlp_GetNpc(Non_1503_Wegelagerer);
+		Npc_SetTempAttitude(weg1503,ATT_HOSTILE);
+		Npc_SetAttitude(weg1503,ATT_HOSTILE);
+		weg1504 = Hlp_GetNpc(Non_1504_Wegelagerer);
+		Npc_SetTempAttitude(weg1504,ATT_HOSTILE);
+		Npc_SetAttitude(weg1504,ATT_HOSTILE);
+	}
+	else
+	{
+	};
 };
-
