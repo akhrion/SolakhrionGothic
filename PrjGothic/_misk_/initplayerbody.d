2565cbaeeeb2f32eb006b586d3977601af2dce2a
Maxim <74652627+akhrion@users.noreply.github.com>
2023-11-30 23:42:33 +0300

    Тест с портированием предметов (доспехов) из Г2 vdfs'ом распаковываем  1.Anims.vdf  сорсером декомпилим нужный MDS-файл Г2   и достаём из него нужный ASC-файл   название этого ASC прописываем в MDS-файле Г1 хранящем    дерево с анимациями доспехов (HUMANS.MDS)  2.Meshes.vdf  нужен аддон Kerrax для блендера (worldofplayers.ru .de)  нужный MRM-файл импортируем в блендер   сразу экспортируем его как 3ds-файл   смотрим какие на нём висят текстуры .tga    названия этих текстур ищем в Textures.vdf     расширение текстур будет .TEX  3. все найденные файлы .ASC, .MDS (отредактированный Г1),.MRM,.3ds,.TEX   кидаем (запаковываем vdfs'ом)    в нашу модификацию .MOD лучше конечно разложить по отдельным томам:   .asc в Anims_mod.mod   .mrm в Meshes_mod.mod   .tex в Textures_mod.mod

--- PrjGothic/_misk_/initplayerbody.d
+++ PrjGothic/_misk_/initplayerbody.d
@@ -1380,3 +1380,32 @@ instance ORG2M(C_Item)
 	count[5] = value;
 };
 
+
+instance ItAr_PAl_H(C_Item)
+{
+	name = "Доспехи паладина";
+	mainflag = ITEM_KAT_ARMOR;
+	flags = 0;
+	protection[PROT_EDGE] = 100;
+	protection[PROT_BLUNT] = 100;
+	protection[PROT_POINT] = 100;
+	protection[PROT_FIRE] = 50;
+	protection[PROT_MAGIC] = 50;
+	value = 0;
+	wear = WEAR_TORSO;
+	visual = "ItAr_Pal_H.3ds";
+	visual_change = "Armor_Pal_H.asc";
+	visual_skin = 0;
+	material = MAT_METAL;
+	description = name;
+	text[1] = NAME_Prot_Edge;
+	count[1] = protection[PROT_EDGE];
+	text[2] = NAME_Prot_Point;
+	count[2] = protection[PROT_POINT];
+	text[3] = NAME_Prot_Fire;
+	count[3] = protection[PROT_FIRE];
+	text[4] = NAME_Prot_Magic;
+	count[4] = protection[PROT_MAGIC];
+	text[5] = NAME_Value;
+	count[5] = value;
+};
