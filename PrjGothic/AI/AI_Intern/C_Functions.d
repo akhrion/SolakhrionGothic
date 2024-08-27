4d39cc6bbf2e6309ceac2c43a66721b3e6dff92f
Maxim <74652627+akhrion@users.noreply.github.com>
2024-08-27 21:33:09 +0300

    Тест: потому-что в описании GetTarget() для деб (меня) написано: (в качестве цели выступает глобальная переменная other)

--- PrjGothic/AI/AI_Intern/C_Functions.d
+++ PrjGothic/AI/AI_Intern/C_Functions.d
@@ -1642,6 +1642,7 @@ func void SpecBehavior()
 			var C_Npc swampfly01;
 			swampfly01 = Hlp_GetNpc(Swampfly_01);
 			if(!Npc_GetTarget(swampfly01)){Npc_SetTarget(swampfly01,swampsharkTarget);};
+			other = Hlp_GetNpc(swampsharkTarget);
 			// PrintSIS(Npc_GetNearestWP(swampshark),Npc_GetTarget(self),other.name);
 
 			return;
